1aed257bee5a3dc71d0481c8c961c35e51a1b35b
Gunkar16 <76874019+Gunkar16@users.noreply.github.com>
2021-07-30 18:33:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+
+var noseX;
+var noseY;
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav")
+	mario_kick = loadSound("kick.wav")
+	mario_gameOver = loadSound("gameover.wav");
+	mario_die = loadSound("mariodie.wav")
+	setSprites();
+	MarioAnimation();
+	img = loadImage("imgs/mario/mario05.png");
+
+	
+}
+
+function setup() {
+	canvas = createCanvas(1240,337);
+	canvas.parent('canvas')
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(600,300);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video,modelLoaded);
+	poseNet.on('pose',gotPoses);
+}
+
+function draw() {
+	game();
+}
+
+function modelLoaded(){
+	console.log("Model Loaded");
+}
+
+function gotPoses(results){
+	if(results.length > 0){
+		console.log(results);
+
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
+}
+
+
+
+
+
+
+
+
